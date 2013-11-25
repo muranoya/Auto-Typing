a37f61cf1d89c64217f09bf80a0dea2d05581537
muraoka daisuke <muranoya@gmail.com>
2013-11-25 13:55:37 +0900

    ファイルを追加
    ファイルを追加した。

---
+++ AutoTyping/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace AutoTyping
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.controlPanel = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.fpsChangeNumericBox = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.capturePBox = new System.Windows.Forms.PictureBox();
+            this.resultPBox = new System.Windows.Forms.PictureBox();
+            this.controlPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fpsChangeNumericBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.capturePBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resultPBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.startButton.Location = new System.Drawing.Point(100, 56);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(51, 23);
+            this.startButton.TabIndex = 5;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.resultTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.resultTextBox.Location = new System.Drawing.Point(12, 6);
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.ReadOnly = true;
+            this.resultTextBox.ShortcutsEnabled = false;
+            this.resultTextBox.Size = new System.Drawing.Size(139, 19);
+            this.resultTextBox.TabIndex = 0;
+            // 
+            // stopButton
+            // 
+            this.stopButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.stopButton.Enabled = false;
+            this.stopButton.Location = new System.Drawing.Point(43, 56);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(51, 23);
+            this.stopButton.TabIndex = 4;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // controlPanel
+            // 
+            this.controlPanel.AutoScroll = true;
+            this.controlPanel.BackColor = System.Drawing.SystemColors.Control;
+            this.controlPanel.Controls.Add(this.label3);
+            this.controlPanel.Controls.Add(this.fpsChangeNumericBox);
+            this.controlPanel.Controls.Add(this.label2);
+            this.controlPanel.Controls.Add(this.stopButton);
+            this.controlPanel.Controls.Add(this.startButton);
+            this.controlPanel.Controls.Add(this.resultTextBox);
+            this.controlPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.controlPanel.Location = new System.Drawing.Point(0, 77);
+            this.controlPanel.Name = "controlPanel";
+            this.controlPanel.Size = new System.Drawing.Size(163, 85);
+            this.controlPanel.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(92, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "ms";
+            // 
+            // fpsChangeNumericBox
+            // 
+            this.fpsChangeNumericBox.Increment = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.fpsChangeNumericBox.Location = new System.Drawing.Point(36, 31);
+            this.fpsChangeNumericBox.Maximum = new decimal(new int[] {
+            1500,
+            0,
+            0,
+            0});
+            this.fpsChangeNumericBox.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.fpsChangeNumericBox.Name = "fpsChangeNumericBox";
+            this.fpsChangeNumericBox.Size = new System.Drawing.Size(50, 19);
+            this.fpsChangeNumericBox.TabIndex = 2;
+            this.fpsChangeNumericBox.ThousandsSeparator = true;
+            this.fpsChangeNumericBox.Value = new decimal(new int[] {
+            400,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Wait";
+            // 
+            // capturePBox
+            // 
+            this.capturePBox.BackColor = System.Drawing.Color.Red;
+            this.capturePBox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.capturePBox.Location = new System.Drawing.Point(0, 0);
+            this.capturePBox.Name = "capturePBox";
+            this.capturePBox.Size = new System.Drawing.Size(163, 44);
+            this.capturePBox.TabIndex = 3;
+            this.capturePBox.TabStop = false;
+            // 
+            // resultPBox
+            // 
+            this.resultPBox.BackColor = System.Drawing.Color.Gray;
+            this.resultPBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.resultPBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.resultPBox.Location = new System.Drawing.Point(0, 44);
+            this.resultPBox.Name = "resultPBox";
+            this.resultPBox.Size = new System.Drawing.Size(163, 33);
+            this.resultPBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.resultPBox.TabIndex = 4;
+            this.resultPBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(163, 162);
+            this.Controls.Add(this.resultPBox);
+            this.Controls.Add(this.capturePBox);
+            this.Controls.Add(this.controlPanel);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "AutoTyping";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.Red;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.controlPanel.ResumeLayout(false);
+            this.controlPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fpsChangeNumericBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.capturePBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resultPBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.TextBox resultTextBox;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Panel controlPanel;
+        private System.Windows.Forms.PictureBox capturePBox;
+        private System.Windows.Forms.PictureBox resultPBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown fpsChangeNumericBox;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ AutoTyping/Form1.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+
+namespace AutoTyping
+{
+    public partial class Form1 : Form
+    {
+        private int fps = 400; //1回あたりの処理時間
+        private bool stopRequest; //Stop要求があればtrue;
+
+        private struct TemplateUnit
+        {
+            private int[,] templateimage;
+            public int[,] TemplateImage
+            {
+                get { return this.templateimage; }
+            }
+
+            private char character;
+            public char Character
+            {
+                get { return this.character; }
+            }
+
+            private int area;
+            public int Threshold
+            {
+                get { return area - 1; }
+            }
+
+            public override string ToString()
+            {
+                return "\"" + Character + "\" " 
+                    + " img:" + TemplateImage.GetLength(0) + "x" + TemplateImage.GetLength(1)
+                    + " Area:" + TemplateImage.GetLength(0) * TemplateImage.GetLength(1);
+            }
+
+            public TemplateUnit(int[,] templateimage, char character)
+            {
+                this.templateimage = templateimage;
+                this.area = templateimage.GetLength(0) * templateimage.GetLength(1);
+                this.character = character;
+            }
+        }
+
+        private struct TemplateResult
+        {
+            private Rectangle machingpoint;
+            public Rectangle MachingPoint
+            {
+                get { return machingpoint; }
+            }
+
+            private char character;
+            public Char Character
+            {
+                get { return character; }
+            }
+
+            public override string ToString()
+            {
+                return "Point:(" + MachingPoint.X + ", " + MachingPoint.Y + ") "
+                    + "Rect:(" + MachingPoint.Width + ", " + MachingPoint.Height + ") "
+                    + "c = " + Character;
+            }
+
+            public TemplateResult(Rectangle rect, char c)
+            {
+                this.machingpoint = rect;
+                this.character = c;
+            }
+        }
+
+        private static void CapcherScreen(Point p, Bitmap bmp)
+        {
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.CopyFromScreen(p.X, p.Y, 0, 0, bmp.Size);
+            }
+        }
+
+        private int[,] GetBinaryImage(Bitmap bmp)
+        {
+            //2値画像を作る
+            int[,] bimg = new int[bmp.Width, bmp.Height];
+            for (int x = 0; x < bmp.Width; x++)
+            {
+                for (int y = 0; y < bmp.Height; y++)
+                {
+                    Color c = bmp.GetPixel(x, y);
+                    int m = (c.R + c.G + c.B) / 3;
+                    bimg[x, y] = (m > 128) ? 0 : 1;
+                }
+            }
+
+            return bimg;
+        }
+
+        //private List<TemplateUnit> MakeFontTemplate()
+        //{
+        //    char[] chars = new char[26 * 1 + 3]; //a-z,-,!,?
+        //    var list = new List<TemplateUnit>();
+
+        //    {
+        //        int index = 0;
+        //        for (int i = 0; i <= 'z' - 'a'; i++, index++)
+        //            chars[index] = (char)('a' + i);
+        //        //for (int i = 0; i <= 'Z' - 'A'; i++, index++)
+        //        //    chars [index] = (char)('A' + i);
+        //        chars[index++] = '!';
+        //        chars[index++] = '-';
+        //        chars[index++] = '?';
+        //    }
+
+        //    Bitmap tmp = new Bitmap(100, 100);
+        //    Graphics g = Graphics.FromImage(tmp);
+        //    foreach (var c in chars)
+        //    {
+        //        g.FillRectangle(Brushes.Black, 0, 0, 100, 100);
+        //        SizeF size = g.MeasureString(c.ToString(), font);
+        //        g.DrawString(c.ToString(), font, Brushes.White, new PointF(0, 0));
+
+        //        Bitmap tmplate = new Bitmap((int)size.Width, (int)size.Height);
+        //        Graphics g2 = Graphics.FromImage(tmplate);
+        //        g2.DrawImageUnscaled(tmp, 0, 0, (int)size.Width, (int)size.Height);
+        //        g2.Dispose();
+
+        //        var bimg = GetBinaryImage(tmplate);
+        //        list.Add(new TemplateUnit(bimg, GetDotsCount(bimg), c));
+        //    }
+        //    g.Dispose();
+        //    tmp.Dispose();
+
+        //    return list;
+        //}
+
+        private List<TemplateUnit> MakeFontTemplate()
+        {
+            var files = Directory.GetFiles("template");
+            var list = new List<TemplateUnit>();
+
+            foreach (var f in files)
+            {
+                string str = Path.GetFileNameWithoutExtension(f);
+                if ("！".Equals(str))
+                {
+                    str = "!";
+                }
+                else if ("？".Equals(str))
+                {
+                    str = "?";
+                }
+                var bmp = GetBinaryImage((Bitmap)Bitmap.FromFile(f));
+                list.Add(new TemplateUnit(bmp, str.ToCharArray()[0]));
+            }
+
+            return list;
+        }
+
+        private string TemplateMatching(List<TemplateUnit> list, int[,] bimg, Bitmap bmp)
+        {
+            int img_width = bimg.GetLength(0);
+            int img_height = bimg.GetLength(1);
+            var result = new List<TemplateResult>();
+            object syncobj = new object();
+
+            Parallel.ForEach(list, (template) =>
+            {
+                int s;
+                int[,] timg = template.TemplateImage;
+                int t_width = timg.GetLength(0);
+                int t_height = timg.GetLength(1);
+
+                for (int x = 0; x < img_width - t_width; x++)
+                {
+                    for (int y = 0; y < img_height - t_height; y++)
+                    {
+                        s = 0;
+                        for (int x2 = 0; x2 < t_width; x2++)
+                        {
+                            for (int y2 = 0; y2 < t_height; y2++)
+                            {
+                                if (timg[x2, y2] == bimg[x + x2, y + y2])
+                                {
+                                    s++;
+                                }
+                            }
+                        }
+                        if (s > template.Threshold)
+                        {
+                            var rect = new Rectangle(x, y, t_width, t_height);
+                            lock (syncobj)
+                            {
+                                result.Add(new TemplateResult(rect, template.Character));
+                                DrawMatchingRect(rect, bmp);
+                            }
+                        }
+                    }
+                }
+            });
+
+            return AlignmentMaching(result);
+        }
+
+        private string AlignmentMaching(List<TemplateResult> list)
+        {
+            string str = "";
+
+            list.Sort((a, b) => a.MachingPoint.X - b.MachingPoint.X);
+
+            foreach (var e in list)
+                str += e.Character;
+
+            return str;
+        }
+
+        private void DrawMatchingRect(Rectangle rect, Bitmap bmp)
+        {
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.DrawRectangle(Pens.Green, rect);
+            }
+        }
+
+        private bool IsIncludeBefore(string before, string after)
+        {
+            if (before.Length > after.Length)
+            {
+                string str = before.Substring(before.Length - after.Length, after.Length);
+                if (str.Equals(after))
+                    return true;
+            }
+            return false;
+        }
+
+        private void AdjustPictureBox()
+        {
+            int height = this.ClientRectangle.Height - controlPanel.Height;
+            capturePBox.Height = height / 2;
+        }
+
+        private void ChangeGUIEnabled(bool isprocessing)
+        {
+            startButton.Enabled = !isprocessing;
+            stopButton.Enabled = isprocessing;
+            label2.Enabled = !isprocessing;
+            label3.Enabled = !isprocessing;
+            fpsChangeNumericBox.Enabled = !isprocessing;
+
+            this.fps = (int)fpsChangeNumericBox.Value;
+            this.stopRequest = !isprocessing;
+
+            if (resultPBox.Image != null)
+                resultPBox.Image.Dispose();
+            resultPBox.Image = null;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            this.stopRequest = true;
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            ChangeGUIEnabled(true);
+
+            //テンプレート作成
+            var template = MakeFontTemplate();
+
+            Task.Factory.StartNew(() =>
+            {
+                int t;
+                Rectangle panel_rect = capturePBox.ClientRectangle;
+                Bitmap bmp = new Bitmap(panel_rect.Width, panel_rect.Height);
+                string before = "";
+
+                for (; !stopRequest; )
+                {
+                    t = Environment.TickCount;
+
+                    //画面をキャプチャ
+                    capturePBox.Invoke(new Action(
+                        () => CapcherScreen(capturePBox.RectangleToScreen(panel_rect).Location, bmp)));
+                    int[,] bimg = GetBinaryImage(bmp);
+
+                    //パターンマッチング
+                    var result = TemplateMatching(template, bimg, bmp);
+                    resultPBox.Invoke(new Action(() =>
+                    {
+                        if (resultPBox.Image != null)
+                            resultPBox.Image.Dispose();
+                        resultPBox.Image = (Image)bmp.Clone();
+                    }));
+
+                    //結果の送信
+                    if (result.Length > 0 && !IsIncludeBefore(before, result))
+                    {
+                        resultTextBox.Invoke(new Action(() => resultTextBox.Text = result));
+                        SendKeys.SendWait(result + "{ENTER}");
+                        before = result;
+
+                        //処理速度にあわせて待機
+                        int span = fps - Environment.TickCount + t;
+                        if (span > 0)
+                            Thread.Sleep(span);
+                    }
+                }
+
+                startButton.Invoke(new Action(() => ChangeGUIEnabled(false)));
+            });
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AdjustPictureBox();
+            this.fpsChangeNumericBox.Value = this.fps;
+            ChangeGUIEnabled(false);
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            AdjustPictureBox();
+        }
+    }
+}
